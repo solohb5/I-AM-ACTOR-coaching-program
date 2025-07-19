6d39986885cf06c72561a3c7362f53e6a833ce6a
Hans Christopher Brofeldt <hansc.brofeldt@Mac.lan>
2025-07-19 12:34:01 -0700

    Fix API exports to use CommonJS for Vercel compatibility

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -28,7 +28,7 @@ const PACKAGES = {
   }
 };
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Only allow POST requests
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
